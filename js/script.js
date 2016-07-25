be4c12f916f6f5f5d845d57416751073d67ee347
NinaSleephead <kx1214@gmail.com>
2016-07-24 21:42:28 -0400

    Advanced-jQuerySelectors-Practise
    Practice for advanced jQuery filters.

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*Nina Kong*/
+$(document).ready(function(){
+	var windowHeight = $( window ).height();
+	$("#Content").height(windowHeight);
+
+    $("h1 + div").css("padding", "5em"); 
+
+    $("div a").css("font-size", "2.5em");
+
+    $("div a.github").css("color", "#b26c34"); 
+
+    $(".github - a").css("color", "#eba96d"); 
+
+    $("#title - h1").css("color", "#b26c34");
+
+    //$("#Content a:first").css("color", "#b37aa5");     
+
+    //$("#Content .linkedin:even").css("color", "#e4b7bf"); 
+
+    //$("#Content a:eq(3)").css("color", "#5278a5");
+
+});
