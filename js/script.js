912887cd87310ed22b0933c75e4d71891511afea
Nina <kx1214@gmail.com>
2016-07-25 20:33:05 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,14 +9,6 @@ $(document).ready(function(){
 
     $("div a.github").css("color", "#b26c34"); 
 
-    $(".github - a").css("color", "#eba96d"); 
-
-    $("#title - h1").css("color", "#b26c34");
-
-    //$("#Content a:first").css("color", "#b37aa5");     
-
-    //$("#Content .linkedin:even").css("color", "#e4b7bf"); 
-
-    //$("#Content a:eq(3)").css("color", "#5278a5");
+    $(".github ~ a").css("color", "#eba96d"); 
 
 });
